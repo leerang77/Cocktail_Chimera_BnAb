1aa1613fe25c46ea8f1612862497ed2d13dad1da
leerang77 <31208793+leerang77@users.noreply.github.com>
2021-07-17 13:24:09 -0400

    Delete UseSharedConstants.m

--- UseSharedConstants.m
+++
@@ -1,6 +0,0 @@
-classdef UseSharedConstants < handle
-%% This class allows access to the shared constants from anywhere
-    properties (Constant) %class property
-       Constants = SharedConstants; 
-    end
-end
